7fe38f83023e51dab2fa23066d2dc044897ed4fb
shimseonjo1 <shimseonjo@naver.com>
2023-02-09 09:45:31 +0900

    notebook 데이터 저장

--- 01_basic/14_notebookPG.py
+++ 01_basic/14_notebookPG.py
@@ -1,13 +1,5 @@
 import notebook as nb
-
-# note_1 = nb.Note('세상 사는 데 도움이 되는 명언')
-# note_2 = nb.Note('삶이 있는 한 희망은 있다')
-
-# notebook_1 = nb.NoteBook('명언노트')
-
-# notebook_1.add_note(note_1)
-# notebook_1.add_note(note_2)
-# print(notebook_1.get_number_of_pages())
+import pickle
 
 '''
 노트북은 하나만 생성할 수 있다.
@@ -15,6 +7,10 @@ import notebook as nb
 1.노트북 생성 2.노트추가 3.노트삭제 4.노트내용 보기 5.종료
 '''
 notebook = None
+
+with open('01_basic/notebook.pickle','rb') as f:
+    notebook = pickle.load(f)
+
 while True:
     menu = input('''
 -------------------------------------------------------
@@ -53,3 +49,7 @@ while True:
     elif menu == '5':
         print('프로그램 종료')
         break
+
+
+with open('01_basic/notebook.pickle','wb') as f:
+    pickle.dump(notebook,f)
