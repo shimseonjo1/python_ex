521414a820b35b18810d2a114bbf642a0bf31225
shimseonjo1 <shimseonjo@naver.com>
2023-02-16 14:35:38 +0900

    고객추가

--- 04_고객관리프로그램/costomer_basic.py
+++ 04_고객관리프로그램/costomer_basic.py
@@ -2,7 +2,6 @@ import re
 custlist=[]
 page=-1
 
-
 while True:
     choice=input('''
     다음 중에서 하실 일을 골라주세요 :
@@ -13,11 +12,46 @@ while True:
     U - 고객 정보 수정
     D - 고객 정보 삭제
     Q - 프로그램 종료
-    ''')  
+    ''').upper()  
 
     if choice=="I":
-        customer={'name':'','gender':'',"email":'',"birthyear":''}        
-        print("고객 정보 입력")
+        customer={'name':'','gender':'',"email":'',"birthyear":''}  
+        customer['name'] = input('이름 >>>')
+        while True:
+            customer['gender'] = input('성별(M,m or F,f) >>> ').upper()
+            if customer['gender'] in ('M','F'):
+                break
+        while True:
+            customer['email'] = input('이메일 >>> ')
+            check = None 
+            for idx, i in enumerate(custlist):
+                if i['email'] == customer['email']:
+                    check=idx
+                    break
+            if check==None:
+                p = re.compile('@[a-z]{2,}[.][a-z]{2,}')
+                if p.search(customer['email'])!=None:
+                    break
+                else:
+                    print('@를 포함한 정확한 이메일을 입력하세요')
+            else:
+                print('중복되는 이메일이 있습니다.')
+
+        while True:
+            customer['birthyear'] = input('출생년도(4자리) >>> ')
+            try:
+                int(customer['birthyear'])
+            except:
+                print('숫자를 입력하세요')
+            else:
+                if len(customer['birthyear']) == 4:
+                    break
+                else:
+                    print('4자리로 입력해주세요')
+        print(customer)
+        custlist.append(customer)
+        page = len(custlist)-1
+
     elif choice=="C":
         print("현재 고객 정보 조회")
     elif choice == 'P':
@@ -31,3 +65,5 @@ while True:
     elif choice=="Q":
         print("프로그램 종료")
         break
+    else:
+        print('메뉴를 잘못선택하셨습니다.')
